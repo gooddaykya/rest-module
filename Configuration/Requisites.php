739ea316b199f3f91350fd467323125b3590ed6e
gooddaykya <makedonskii.net@gmail.com>
2017-08-15 14:31:41 +0300

    Requisites :  adding Requisites class

---
+++ Configuration/Requisites.php
@@ -0,0 +1,20 @@
+<?php
+	namespace NSPACE/Configuration
+
+	class Requisites
+	{
+		public function __construct()
+		{
+			$this->host = 'localhost',
+			$this->user = 'root',
+			$this->pass = '',
+			$this->name = 'test_base',
+			$this->char = 'utf8';
+		}
+
+		public function __call($requisiteName, $val = null)
+		{
+			if (isset($this->$requisiteName))
+				return $this->$requisiteName;
+		}
+	}
