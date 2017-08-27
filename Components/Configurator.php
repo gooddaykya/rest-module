969c3bbc7f955c676be321e86497f3bd577792b3
gooddaykya <makedonskii.net@gmail.com>
2017-08-27 23:10:53 +0300

    REST :  created configurator

---
+++ Components/Configurator.php
@@ -0,0 +1,22 @@
+<?php
+	namespace REST\Components;
+
+	class Configurator
+	{
+		private $id;
+		private $body;
+
+		public function __construct(int $id = null, $body = array())
+		{
+			$this->id = $id;
+			$this->body = $body;
+		}
+
+		public function __call($prop, $params = null)
+		{
+			if (property_exists($this->{$prop}))
+				return $this->{$prop};
+			else
+				throw new \Exception('Configurator :  unknown property' . PHP_EOL);
+		}
+	}
