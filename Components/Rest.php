a84d91967750d4dd2644cfb83961e5d48b23dce0
gooddaykya <makedonskii.net@gmail.com>
2017-08-27 23:04:17 +0300

    REST :  patched Rest parent, +properties. Patched constructor, added dependencies

--- Components/Rest.php
+++ Components/Rest.php
@@ -3,7 +3,21 @@
 
 	abstract class Rest implements \REST\Interfaces\RESTAdaper
 	{
-		public abstract function configure();
+		protected $model;
+		protected $conf;
+		protected $result;
+
+		public function __construct(\REST\Interfaces\RESTModel $model,
+									\REST\Components\Configurator $conf)
+		{
+			$this->model = $model;
+			$this->conf  = $conf;
+		}
+
+		public function getResponse()
+		{
+			return $this->result;
+		}
+		
 		public abstract function execute();
-		public abstract function getResponse();
 	}
