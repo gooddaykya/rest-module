24ea665c1c1c06d1b13773f2259f59418c6e61d6
gooddaykya <makedonskii.net@gmail.com>
2017-08-28 00:55:28 +0300

    restFactory :  created factory

---
+++ Components/RestFactory.php
@@ -0,0 +1,40 @@
+<?php
+	namespace REST\Components;
+	use namespace \REST\Components\Rest as Rest;
+
+	class RestFactory
+	{
+		public static function create(\REST\Interfaces\RESTModelAdapter $model,
+									  int $id = null,
+									  array $body = null)
+		{
+			$conf = new \REST\Components\Configurator($id, $body);
+			$rest = null;
+
+			$method = $_SERVER['REQUEST_METHOD'];
+
+			switch ($method)
+			{
+				case 'GET':
+					$rest = new Rest\GET($model, $conf);
+					break;
+
+				case 'PUT':
+					$rest = new Rest\PUT($model, $conf);
+					break;
+
+				case 'POST':
+					$rest = new Rest\POST($model, $conf);
+					break;
+
+				case 'DELETE':
+					$rest = new Rest\DELETE($model, $conf);
+					break;
+
+				default:
+					throw new \Exception('REST Factory :  undefined method used' . PHP_EOL);
+			}
+
+			return $rest;
+		}
+	}
